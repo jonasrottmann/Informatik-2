c5d0b238a852b586dcb32cc10db44a311f242fe4
jonas <jonas@jonas-mbp.fritz.box>
2014-03-18 13:57:59 +0100

    Initial revision

---
+++ src/aufgabe_3_1_gameoflife/GameOfLifeLogic.java
@@ -0,0 +1,19 @@
+package aufgabe_3_1_gameoflife;
+/**
+ * Die eigentliche Spielelogik. Das Spielfeld wird hier nicht
+ * als zyklisch geschlossen betrachtet wird.
+ *
+ * @author Holger Vogelsang
+ */
+public class GameOfLifeLogic {
+
+	public void setStartGeneration(boolean[][] generation) {
+	}
+
+	public void nextGeneration() {
+	}
+
+	public boolean isCellAlive(int x, int y) {
+		return false;
+	}
+}
